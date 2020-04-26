b585103250f17dd7585ce4d13518c49982e3b8b0
shehab.nagy1998@gmail.com <shehabnagy1998>
2020-04-26 18:23:04 +0200

    initial commit

---
+++ src/containers/Topbar/HorizontalNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+import IntlMessages from "../../util/IntlMessages";
+
+const HorizontalNav = () => {
+  return (
+    <ul className="navbar-list">
+      <li className="navbar-list-item">
+        <Link to="/home">
+          <IntlMessages id="sidebar.home" />
+        </Link>
+      </li>
+      <li className="navbar-list-item">
+        <Link to="/home">
+          <IntlMessages id="sidebar.strategies" />
+        </Link>
+      </li>
+      <li className="navbar-list-item">
+        <Link to="/home">
+          <IntlMessages id="sidebar.orders" />
+        </Link>
+      </li>
+      <li className="navbar-list-item">
+        <Link to="/home">
+          <IntlMessages id="sidebar.stats" />
+        </Link>
+      </li>
+      <li className="navbar-list-item">
+        <Link to="/home">
+          <IntlMessages id="sidebar.inVoices" />
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+HorizontalNav.propTypes = {};
+
+export default HorizontalNav;
